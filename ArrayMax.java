7b38838478f3659bd1e0c82c5aaf2f12d70129d3
Neetesh Thakur <114156149+Neetesh-6@users.noreply.github.com>
2023-03-15 09:32:35 +0530

    Add files via upload

---
+++ ArrayMax.java
@@ -0,0 +1,26 @@
+package arraymax;
+
+import java.util.Scanner;
+
+public class ArrayMax {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the size of the integer array: ");
+	        int size = sc.nextInt();
+	        System.out.println("Enter the array integers: ");
+	        int[] arr = new int[size];
+	        for (int i = 0; i < size; i++) {
+	            arr[i] = sc.nextInt();
+
+	        }
+	        int max = arr[0];
+	        for (int i = 1; i < size; i++) {
+	            if (arr[i] > max) {
+	                max = arr[i];
+	            }
+
+	        }
+	        System.out.println("Maximum element is: " + max);
+
+	    }
+	}
